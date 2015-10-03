fcc2fc9920edbf3702623ad8cc3e3a24944cce58
Ben <benjheng@gmail.com>
2015-10-04 03:10:50 +0800

    Add smooth scrolling
    Less visually jarring for the user, since the task list doesn't
    immediately jump to the next scroll position.

--- src/app/view/TaskListViewManager.java
+++ src/app/view/TaskListViewManager.java
@@ -12,6 +12,9 @@ import app.model.Task;
 import app.model.TaskCell;
 import app.model.TaskList;
 import app.util.LogHelper;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -21,6 +24,7 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollBar;
 import javafx.util.Callback;
+import javafx.util.Duration;
 
 public class TaskListViewManager {
 	private ViewManager viewManager;
@@ -116,11 +120,21 @@ public class TaskListViewManager {
 		Node node = taskListViewLayout.lookup(".scroll-bar");
 		if (node instanceof ScrollBar) {
 			ScrollBar scrollBar = (ScrollBar) node;
+			double newValue = scrollBar.getValue();
 			if (direction == ScrollDirection.UP && scrollBar.getValue() != 0) {
-				scrollBar.setValue(scrollBar.getValue() - step);
+				newValue = scrollBar.getValue() - step;
 			} else if (direction == ScrollDirection.DOWN && scrollBar.getValue() != scrollBar.getMax()) {
-				scrollBar.setValue(scrollBar.getValue() + step);
+				newValue = scrollBar.getValue() + step;
 			}
+
+			// TODO: if this animation is used elsewhere, refactor to Common or
+			// something.
+			// Animate scroll to provide smooth scrolling
+			Timeline timeline = new Timeline();
+			KeyValue kv = new KeyValue(scrollBar.valueProperty(), newValue);
+			KeyFrame kf = new KeyFrame(Duration.millis(200), kv);
+			timeline.getKeyFrames().add(kf);
+			timeline.play();
 		}
 	}
 
