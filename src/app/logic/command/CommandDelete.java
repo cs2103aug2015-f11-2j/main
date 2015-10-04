49d7c6e85c8bebae516a8badc6541051d3caac4f
Kenny <kyuen@nus.edu.sg>
2015-10-05 05:47:57 +0800

    Operation-Delete-bug fix
    Bug Fix report:
    1) Allow MULTIPLE task delete (eg: delete 2,4,1)
    2) Throw error message if duplicates found on multiple task delete (eg
    delete 1,2,3,4,1,2)

--- src/app/logic/command/CommandDelete.java
+++ src/app/logic/command/CommandDelete.java
@@ -2,6 +2,7 @@ package app.logic.command;
 
 import app.constants.ViewConstants;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.UUID;
 import app.constants.CommandConstants.CommandType;
 import app.constants.ViewConstants.StatusType;
@@ -18,65 +19,61 @@ public class CommandDelete extends Command {
 		super();
 		this.setCommandType(CommandType.REMOVE);
 	}
-	
+
 	@Override
 	public ViewState execute(ViewState previousViewState) {
 		LogHelper.getLogger().info("Executing CommandDelete object.");
 		ViewState viewState = new ViewState();
-		
+
 		if (this.getContent().isEmpty()) {
 			viewState.setStatus(StatusType.ERROR, ViewConstants.ERROR_DELETE_NO_TASK);
 			return viewState;
 		}
-		
+
 		try {
-			// master task list contains all tasks in memory
 			TaskList master = CommandController.getInstance().getMasterTaskList();
-			
-			// displayed task list is the task list currently being shown to the user
-			// this is the task list the user is using to select the tasks he wants to delete
 			TaskList display = previousViewState.getTaskList();
 			
-			// get the ids the user specified. 
-			// example: if the command is "delete 1 3 4" , this method will give you give [1, 3, 4]
-			ArrayList<Integer> ids = Common.getIdArrayList(this.getContent());
-			
-			// find the Task objects that these ids (1,3,4) correspond to, inside the displayed task list
-			// The tasks here are identified by their UUID (unique identifier)
+			ArrayList<Integer> ids = Common.getIdArrayList(this.getContent()); 
 			ArrayList<UUID> tasksUuidList = display.getTasksUuidList(ids);
-			
-			
-			// Use the UUIDs to find the indexes of the tasks in the master task list.
-			// indexes as in, from 0 to master.size()
 			ArrayList<Integer> masterIdsList = master.getTasksIdList(tasksUuidList);
+
+			Collections.sort(ids, Collections.reverseOrder());
+			Collections.sort(masterIdsList, Collections.reverseOrder());
+
 			
-			
-			// remove task from master list
-			for( int i : masterIdsList){
-				master.getTaskList().remove(i);
+			// check for identical id's 
+			for (int i = ids.size() - 1; i > 0; i--) {
+				if (ids.get(i).intValue() == ids.get(i - 1).intValue()) {
+					viewState.setStatus(StatusType.ERROR, String.format(ViewConstants.ERROR_DELETE, "Duplicated values detected"));
+					return viewState;
+				}
 			}
-			
-			
-			// store message
-			String message = display.getTaskList().get(Integer.parseInt(this.getContent())-1).getName();
-			
+
 			
 			// remove task from display list
-			display.getTaskList().remove(Integer.parseInt(this.getContent())-1);
+			for (int i : ids) {
+				display.getTaskList().remove(i - 1);
+			}
 
 			
+			// remove task from master list
+			for (int i : masterIdsList) {
+				master.getTaskList().remove(i);
+			}
+
+
 			viewState.setTaskList(display);
-			viewState.setStatus(StatusType.SUCCESS, String.format(ViewConstants.MESSAGE_DELETE, message));
+			viewState.setStatus(StatusType.SUCCESS, String.format(ViewConstants.MESSAGE_DELETE, this.getContent()));
 			setExecuted(true);
 
-			
 		} catch (Exception e) {
 			LogHelper.getLogger().severe(e.getMessage());
 			viewState.setStatus(StatusType.ERROR, String.format(ViewConstants.ERROR_DELETE, this.getContent()));
 		}
-		
+
 		viewState.setActiveView(ViewType.TASK_LIST);
 		return viewState;
 	}
-	
+
 }
