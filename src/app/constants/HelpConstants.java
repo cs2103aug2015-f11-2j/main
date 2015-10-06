5e0efa297b2c880d044faec9e35a53476eb8a825
Ben <benjheng@gmail.com>
2015-10-07 00:36:11 +0800

    Add info view for edit command

--- src/app/constants/HelpConstants.java
+++ src/app/constants/HelpConstants.java
@@ -6,5 +6,10 @@ public class HelpConstants {
 	public static final String HELP_ADD_OVERVIEW = "add <task> [from <date> to <date>] [by <date>] [priority high | medium | low]";
 	public static final String HELP_ADD_EXAMPLES = "add do tutorials by sunday 10pm\n"
 			+ "add math exam from 26/11/15 1pm to 3pm priority high";
-	
+
+	// EDIT
+	public static final String HELP_EDIT_DESCRIPTION = "Edits a task";
+	public static final String HELP_EDIT_OVERVIEW = "edit <id> [task] [from <date> to <date>] [by <date>] [priority high | medium | low]";
+	public static final String HELP_EDIT_EXAMPLES = "";
+
 }
