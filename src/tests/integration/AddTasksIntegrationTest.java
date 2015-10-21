002942aedb32381a33b46c69114cd32e7b4735c6
Ben <benjheng@gmail.com>
2015-10-21 19:56:51 +0800

    Refactor unnecessarily convoluted code

--- src/tests/integration/AddTasksIntegrationTest.java
+++ src/tests/integration/AddTasksIntegrationTest.java
@@ -2,12 +2,9 @@ package tests.integration;
 
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-
 import org.junit.Test;
 
 import app.Main;
-import app.logic.command.CommandExit;
 import app.storage.AppStorage;
 
 public class AddTasksIntegrationTest {
@@ -16,11 +13,12 @@ public class AddTasksIntegrationTest {
 	public void testAddTasks() throws Exception {
 		String prevStorageLocation = AppStorage.getInstance().getStorageFileLocation();
 		AppStorage.getInstance().setStorageFileLocation("testStorage/integration1.txt");
-
-		String[] commands = Arrays.asList("add first task", 
+		
+		String[] commands = {
+			"add first task", 
 			"add second task",
 			"exit"
-		).stream().toArray(String[]::new);
+		};
 		
 		try {
 			Main.main(commands);
