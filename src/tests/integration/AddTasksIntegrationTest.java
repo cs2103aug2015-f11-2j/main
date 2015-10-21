b5ef79bf7d172c9719bff25ec0be98e8abe0f05f
fionachang <fiona.chang.w@gmail.com>
2015-10-21 23:28:45 +0800

    Added methods to create and delete temp files for integration testing

--- src/tests/integration/AddTasksIntegrationTest.java
+++ src/tests/integration/AddTasksIntegrationTest.java
@@ -2,10 +2,18 @@ package tests.integration;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.Test;
 
 import app.Main;
+import app.model.TaskList;
 import app.storage.AppStorage;
+import app.storage.TaskStorage;
 
 public class AddTasksIntegrationTest {
 
@@ -13,6 +21,7 @@ public class AddTasksIntegrationTest {
 	public void testAddTasks() throws Exception {
 		String prevStorageLocation = AppStorage.getInstance().getStorageFileLocation();
 		AppStorage.getInstance().setStorageFileLocation("testStorage/integration1.txt");
+		File testFile = createTestFile();
 		
 		String[] commands = {
 			"add first task", 
@@ -26,9 +35,53 @@ public class AddTasksIntegrationTest {
 		} catch (Exception e) {
 			throw e; // JUnit will handle this and report a failed assertion
 		} finally {
+			removeFileAndParentsIfEmpty(testFile.toPath());
 			AppStorage.getInstance().setStorageFileLocation(prevStorageLocation);
 		}
 
 	}
 
+	private File createTestFile() {
+		File testFile = new File(AppStorage.getInstance().getStorageFileLocation());
+		
+		if (!testFile.exists()) {
+			if (testFile.getParentFile() != null) {
+				testFile.getParentFile().mkdirs();
+			}
+
+			try {
+				testFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			TaskStorage.getInstance().writeTasks(new TaskList());
+		}
+
+		return testFile;
+	}
+
+	private void removeFileAndParentsIfEmpty(Path path) throws IOException {
+		if (path == null) {
+			return;
+		}
+
+		File file = path.toFile();
+
+		if (Files.isRegularFile(path)) {
+			Files.deleteIfExists(path);
+		} else if (Files.isDirectory(path)) {
+			if (file.list().length != 0) {
+				return;
+			}
+
+			try {
+				Files.delete(path);
+			} catch (DirectoryNotEmptyException e) {
+				return;
+			}
+		}
+
+		removeFileAndParentsIfEmpty(path.getParent());
+	}
 }
