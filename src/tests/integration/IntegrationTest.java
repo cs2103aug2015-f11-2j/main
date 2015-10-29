c85de58bc79a99f2454c62e895a9dd8dd5ac9f61
Ben <benjheng@gmail.com>
2015-10-29 20:07:09 +0800

    Add integration test

--- src/tests/integration/IntegrationTest.java
+++ src/tests/integration/IntegrationTest.java
@@ -7,15 +7,18 @@ import java.io.IOException;
 import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 import org.junit.Test;
 
 import app.Main;
+import app.constants.TaskConstants.Priority;
+import app.model.Task;
 import app.model.TaskList;
 import app.storage.AppStorage;
 import app.storage.TaskStorage;
 
-public class AddTasksIntegrationTest {
+public class IntegrationTest {
 
 	@Test
 	public void testAddTasks() throws Exception {
@@ -24,14 +27,29 @@ public class AddTasksIntegrationTest {
 		File testFile = createTestFile();
 		
 		String[] commands = {
-			"add first task", 
-			"add second task",
+			"add 1st task", 
+			"add 2nd task",
+			"add 3rd task",
+			"add 4th task",
+			"edit 1 priority high",
+			"mark 2 3",
+			"display completed",
+			"delete 1",
 			"exit"
 		};
 		
+		/* Final result:
+		 * 1st task (priority high)
+		 * 3rd task (completed)
+		 * 4th task
+		 */
 		try {
 			Main.main(commands);
-			// TODO: assertions here to ensure commands are successful.
+			List<Task> tasks = TaskStorage.getInstance().readTasks().getTaskList();
+			assertEquals(3, tasks.size());
+			assertEquals(Priority.HIGH, tasks.get(0).getPriority());
+			assertTrue(tasks.get(1).isCompleted());
+			assertEquals("4th task", tasks.get(2).getName());
 		} catch (Exception e) {
 			throw e; // JUnit will handle this and report a failed assertion
 		} finally {
