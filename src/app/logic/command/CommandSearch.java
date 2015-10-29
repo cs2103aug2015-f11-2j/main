231c371940ec1fed8a019c9da9733584a3db87ab
Lee Ming Xuan <lmx@u.nus.edu>
2015-10-29 14:38:22 +0800

    Fix for #79
    Problem is by default search function searches the uncompleted
    taskslist.
    Current solution is to remove the no parameters error and thus by just
    simply typing search, uncompleted tasks are displayed.

--- src/app/logic/command/CommandSearch.java
+++ src/app/logic/command/CommandSearch.java
@@ -32,47 +32,44 @@ public class CommandSearch extends Command {
 		TaskList master = CommandController.getInstance().getMasterTaskList();
 		TaskList retrievedTaskList = master.getTaskListByCompletion(false);
 
-
-		LogHelper.getInstance().getLogger().info("\nContent: " + this.getContent() + "\nPriority: " + this.getPriority() + "\nStart: "
-				+ this.getStartDate() + "\nEnd: " + this.getEndDate() + "\nType: " + this.getDisplayType());
+		LogHelper.getInstance().getLogger()
+				.info("\nContent: " + this.getContent() + "\nPriority: " + this.getPriority() + "\nStart: "
+						+ this.getStartDate() + "\nEnd: " + this.getEndDate() + "\nType: " + this.getDisplayType());
 
 		try {
-		if (this.getDisplayType() == DisplayType.ALL) {
-			retrievedTaskList = master;
-			LogHelper.getInstance().getLogger().info("All");
-		} else if (this.getDisplayType() == DisplayType.COMPLETED) {
-			retrievedTaskList = master.getTaskListByCompletion(true);
-			LogHelper.getInstance().getLogger().info("Comp");
-		}
-
-		List<Predicate<Task>> predicates = new ArrayList<Predicate<Task>>();
-		if(!this.getContent().isEmpty()) {
-			predicates.add(Predicates.keywordMatches(this.getContent()));
-		}
-		if (!this.getPriority().equals(Priority.NONE)) {
-			predicates.add(Predicates.priorityEquals(this.getPriority()));
-		}
-		if (!(this.getStartDate() == null) && !(this.getEndDate() == null)) {
-			predicates.add(Predicates.betweenDates(this.getStartDate(), this.getEndDate()));
-		} else {
-			if (this.getEndDate() != null) {
-				predicates.add(Predicates.endDateBefore(this.getEndDate()));
+			LogHelper.getInstance().getLogger().info("HERE: " + this.getDisplayType());
+			if (this.getDisplayType() == DisplayType.ALL) {
+				retrievedTaskList = master;
+				LogHelper.getInstance().getLogger().info("All");
+			} else if (this.getDisplayType() == DisplayType.COMPLETED) {
+				retrievedTaskList = master.getTaskListByCompletion(true);
+				LogHelper.getInstance().getLogger().info("Comp");
 			}
-			if (this.getStartDate() != null) {
-				predicates.add(Predicates.startDateAfter(this.getStartDate()));
-			}	
-		}
-		if (predicates.isEmpty()) {
-			viewState.setStatus(StatusType.ERROR, ViewConstants.ERROR_SEARCH_NO_PARAMETER);
-			return viewState;
-		}
 
-		TaskList results = retrievedTaskList.search(predicates);
-		viewState.setTaskList(results);
-		viewState.setHeader(ViewConstants.HEADER_SEARCH);
-		viewState.setStatus(StatusType.SUCCESS,
-				String.format(ViewConstants.SEARCH_MESSAGE, results.getTaskList().size()));
-		this.setExecuted(true);
+			List<Predicate<Task>> predicates = new ArrayList<Predicate<Task>>();
+			if (!this.getContent().isEmpty()) {
+				predicates.add(Predicates.keywordMatches(this.getContent()));
+			}
+			if (!this.getPriority().equals(Priority.NONE)) {
+				predicates.add(Predicates.priorityEquals(this.getPriority()));
+			}
+			if (!(this.getStartDate() == null) && !(this.getEndDate() == null)) {
+				predicates.add(Predicates.betweenDates(this.getStartDate(), this.getEndDate()));
+			} else {
+				if (this.getEndDate() != null) {
+					predicates.add(Predicates.endDateBefore(this.getEndDate()));
+				}
+				if (this.getStartDate() != null) {
+					predicates.add(Predicates.startDateAfter(this.getStartDate()));
+				}
+			}
+
+			TaskList results = retrievedTaskList.search(predicates);
+			viewState.setTaskList(results);
+			viewState.setHeader(ViewConstants.HEADER_SEARCH);
+			viewState.setStatus(StatusType.SUCCESS,
+					String.format(ViewConstants.SEARCH_MESSAGE, results.getTaskList().size()));
+			this.setExecuted(true);
 		} catch (Exception e) {
 			LogHelper.getInstance().getLogger().severe(e.getMessage());
 			viewState.setStatus(StatusType.ERROR, String.format(ViewConstants.ERROR_DELETE, this.getContent()));
