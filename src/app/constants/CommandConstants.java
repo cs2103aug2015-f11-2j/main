d3f8b0e587fce9eb20590c4ef2f4d0861ce78391
Ben <benjheng@gmail.com>
2015-11-05 19:47:03 +0800

    Revert "#85 suggestion"
    This reverts commit 8a8e48b5c129731bd7e7dbe575de225ec98e0e62.

--- src/app/constants/CommandConstants.java
+++ src/app/constants/CommandConstants.java
@@ -8,16 +8,16 @@ public class CommandConstants {
 
 	// Aliases for commands
 	public static final List<String> ALIASES_ADD = Common.getUnmodifiableList("add", "ad", "a", "+");
-	public static final List<String> ALIASES_REMOVE = Common.getUnmodifiableList("remove", "delete", "rm", "-", "de", "del", "dele", "delet");
+	public static final List<String> ALIASES_REMOVE = Common.getUnmodifiableList("remove", "delete", "rm", "-");
 	public static final List<String> ALIASES_HELP = Common.getUnmodifiableList("help", "?");
-	public static final List<String> ALIASES_THEME = Common.getUnmodifiableList("theme", "th", "the", "them");
+	public static final List<String> ALIASES_THEME = Common.getUnmodifiableList("theme");
 	public static final List<String> ALIASES_EXIT = Common.getUnmodifiableList("exit", "quit");
-	public static final List<String> ALIASES_MARK = Common.getUnmodifiableList("mark", "m", "ma", "mar");
-	public static final List<String> ALIASES_DISPLAY = Common.getUnmodifiableList("display", "show", "view", "di", "dis", "disp", "displ", "displa");
+	public static final List<String> ALIASES_MARK = Common.getUnmodifiableList("mark", "m");
+	public static final List<String> ALIASES_DISPLAY = Common.getUnmodifiableList("display", "show", "view");
 	public static final List<String> ALIASES_EDIT = Common.getUnmodifiableList("edit", "modify", "change", "update", "ed", "edi");
-	public static final List<String> ALIASES_SEARCH = Common.getUnmodifiableList("search", "s", "se", "sea", "sear", "searc");
-	public static final List<String> ALIASES_SAVE = Common.getUnmodifiableList("save", "sa", "sav");
-	public static final List<String> ALIASES_UNDO = Common.getUnmodifiableList("undo", "u", "un", "und");
+	public static final List<String> ALIASES_SEARCH = Common.getUnmodifiableList("search", "s");
+	public static final List<String> ALIASES_SAVE = Common.getUnmodifiableList("save");
+	public static final List<String> ALIASES_UNDO = Common.getUnmodifiableList("undo");
 
 	// Enum values for the different types of commands
 	public enum CommandType {
