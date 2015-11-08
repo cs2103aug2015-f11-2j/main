48386d8373a3c37e05b2bb8bc2ca1beba7d7321f
Ben <benjheng@gmail.com>
2015-11-08 19:36:59 +0800

    Add 'final' keyword to constants

--- src/app/constants/StorageConstants.java
+++ src/app/constants/StorageConstants.java
@@ -3,28 +3,28 @@ package app.constants;
 // @@author A0125960E
 public class StorageConstants {
 	// file locations
-	public static String FILE_CONFIGURATION = "config.properties";
-	public static String FILE_DEFAULT_STORAGE = "next.txt";
-	public static String FILE_DEFAULT_LOG = "logs/next.log";
+	public static final String FILE_CONFIGURATION = "config.properties";
+	public static final String FILE_DEFAULT_STORAGE = "next.txt";
+	public static final String FILE_DEFAULT_LOG = "logs/next.log";
 
 	// configuration file properties
-	public static String PROPERTIES_STORAGE_FILE_LOCATION = "storageFileLocation";
-	public static String PROPERTIES_LOG_FILE_LOCATION = "logFileLocation";
-	public static String PROPERTIES_SELECTED_THEME = "selectedTheme";
+	public static final String PROPERTIES_STORAGE_FILE_LOCATION = "storageFileLocation";
+	public static final String PROPERTIES_LOG_FILE_LOCATION = "logFileLocation";
+	public static final String PROPERTIES_SELECTED_THEME = "selectedTheme";
 	
 	// index of observers in the observer list
-	public static int OBSERVER_INDEX_STORAGE = 0;
-	public static int OBSERVER_INDEX_LOG = 1;
+	public static final int OBSERVER_INDEX_STORAGE = 0;
+	public static final int OBSERVER_INDEX_LOG = 1;
 
 	// error messages
-	public static String ERROR_INITIALIZE_TASKSTORAGE = "IOException while initializing TaskStorage";
-	public static String ERROR_WRITE_PROPERTIES = "IOException while writing properties to configuration file";
-	public static String ERROR_WRITE_TASKS = "IOException while writing tasks to storage file";
-	public static String ERROR_READ_PROPERTIES = "IOException while reading properties from configuration file";
-	public static String ERROR_READ_TASKS = "IOException while reading tasks from storage file";
-	public static String ERROR_TO_CANONICAL_PATH = "IOException while converting path to canonical";
+	public static final String ERROR_INITIALIZE_TASKSTORAGE = "IOException while initializing TaskStorage";
+	public static final String ERROR_WRITE_PROPERTIES = "IOException while writing properties to configuration file";
+	public static final String ERROR_WRITE_TASKS = "IOException while writing tasks to storage file";
+	public static final String ERROR_READ_PROPERTIES = "IOException while reading properties from configuration file";
+	public static final String ERROR_READ_TASKS = "IOException while reading tasks from storage file";
+	public static final String ERROR_TO_CANONICAL_PATH = "IOException while converting path to canonical";
 	
-	public static String LOG_USER_INPUT = "User pressed enter key with input: %1$s";
-	public static String LOG_INITIALIZE_VIEW = "Initializing view: %1$s";
-	public static String LOG_EXECUTE_COMMAND = "Executing %1$s object";
+	public static final String LOG_USER_INPUT = "User pressed enter key with input: %1$s";
+	public static final String LOG_INITIALIZE_VIEW = "Initializing view: %1$s";
+	public static final String LOG_EXECUTE_COMMAND = "Executing %1$s object";
 }
--- src/app/constants/ViewConstants.java
+++ src/app/constants/ViewConstants.java
@@ -9,105 +9,105 @@ public class ViewConstants {
 
 	// @@author A0126120B
 	// Style classes for the different statusbar statuses
-	public static String STATUS_STYLE_SUCCESS = "success";
-	public static String STATUS_STYLE_ERROR = "error";
-	public static String STATUS_STYLE_INFO = "info";
+	public static final String STATUS_STYLE_SUCCESS = "success";
+	public static final String STATUS_STYLE_ERROR = "error";
+	public static final String STATUS_STYLE_INFO = "info";
 	
 	// @@author A0126120B
 	// Style classes for task item colors
-	public static String ITEM_PURPLE = "purple";
-	public static String ITEM_BLUE = "blue";
-	public static String ITEM_GREEN = "green";
-	public static String ITEM_YELLOW = "yellow";
-	public static String ITEM_ORANGE = "orange";
-	public static String ITEM_RED = "red";
-	public static String ITEM_PINK = "pink";
+	public static final String ITEM_PURPLE = "purple";
+	public static final String ITEM_BLUE = "blue";
+	public static final String ITEM_GREEN = "green";
+	public static final String ITEM_YELLOW = "yellow";
+	public static final String ITEM_ORANGE = "orange";
+	public static final String ITEM_RED = "red";
+	public static final String ITEM_PINK = "pink";
 
 	// @@author A0126120B
 	// THEME command
 	// Resource locations for the theme CSS files.
-	public static String THEME_LIGHT_CSS = Main.class.getResource("view/css/theme_light.css").toExternalForm();
-	public static String THEME_DARK_CSS = Main.class.getResource("view/css/theme_dark.css").toExternalForm();
-	public static String THEME_LIGHT = "light";
-	public static String THEME_DARK = "dark";
-	public static String MESSAGE_AVAILABLE_THEMES = "Available themes: light, dark";
-	public static String MESSAGE_CURRENT_THEME = "Current theme: %1$s";
+	public static final String THEME_LIGHT_CSS = Main.class.getResource("view/css/theme_light.css").toExternalForm();
+	public static final String THEME_DARK_CSS = Main.class.getResource("view/css/theme_dark.css").toExternalForm();
+	public static final String THEME_LIGHT = "light";
+	public static final String THEME_DARK = "dark";
+	public static final String MESSAGE_AVAILABLE_THEMES = "Available themes: light, dark";
+	public static final String MESSAGE_CURRENT_THEME = "Current theme: %1$s";
 	
 	// @@author A0126120B
 	// ADD command
-	public static String MESSAGE_ADD = "Added task: %1$s";
-	public static String ERROR_ADD = "Error adding task: %1$s";
-	public static String ERROR_ADD_NO_TASK = "No task specified";
+	public static final String MESSAGE_ADD = "Added task: %1$s";
+	public static final String ERROR_ADD = "Error adding task: %1$s";
+	public static final String ERROR_ADD_NO_TASK = "No task specified";
 	
 	// @@author A0125990Y
 	// DELETE command
-	public static String MESSAGE_DELETE = "Deleted task: %1$s";
-	public static String ERROR_DELETE = "Error deleting task: %1$s";
-	public static String ERROR_DELETE_NO_TASK = "No task specified";
-	public static String ERROR_DELETE_INVALID_ID = "Invalid task ID entered";
+	public static final String MESSAGE_DELETE = "Deleted task: %1$s";
+	public static final String ERROR_DELETE = "Error deleting task: %1$s";
+	public static final String ERROR_DELETE_NO_TASK = "No task specified";
+	public static final String ERROR_DELETE_INVALID_ID = "Invalid task ID entered";
 	
 	// @@author A0125360R
 	// MARK command
-	public static String MESSAGE_MARK_COMPLETED = "Marked %s completed: %s";
-	public static String MESSAGE_MARK_UNCOMPLETED = "Marked %s uncompleted: %s";
-	public static String ERROR_MARK = "Error marking task";
-	public static String ERROR_MARK_NO_TASK = "No task specified to mark";
-	public static String ERROR_MARK_INVALID_ID = "Invalid task ID entered";
+	public static final String MESSAGE_MARK_COMPLETED = "Marked %s completed: %s";
+	public static final String MESSAGE_MARK_UNCOMPLETED = "Marked %s uncompleted: %s";
+	public static final String ERROR_MARK = "Error marking task";
+	public static final String ERROR_MARK_NO_TASK = "No task specified to mark";
+	public static final String ERROR_MARK_INVALID_ID = "Invalid task ID entered";
 	
 	// @@author A0125360R
 	// DISPLAY command
-	public static String MESSAGE_DISPLAY = "Displaying %1$s tasks";
-	public static String ERROR_DISPLAY = "Error displaying tasks";
-	public static String ERROR_DISPLAY_INVALID_ARGUMENT = "Invalid option. Available: all, completed, uncompleted (default)";
+	public static final String MESSAGE_DISPLAY = "Displaying %1$s tasks";
+	public static final String ERROR_DISPLAY = "Error displaying tasks";
+	public static final String ERROR_DISPLAY_INVALID_ARGUMENT = "Invalid option. Available: all, completed, uncompleted (default)";
 	
 	// @@author A0125360R
 	// EDIT command
-	public static String MESSAGE_EDIT = "Edited task: %1$s";
-	public static String ERROR_EDIT = "Error editing task";
-	public static String ERROR_EDIT_NO_TASK = "No task specified to edit";
-	public static String ERROR_EDIT_INVALID_TASK_ID = "Invalid task ID entered";
-	public static String ERROR_EDIT_NO_CHANGES = "No changes made for task %1$s";
+	public static final String MESSAGE_EDIT = "Edited task: %1$s";
+	public static final String ERROR_EDIT = "Error editing task";
+	public static final String ERROR_EDIT_NO_TASK = "No task specified to edit";
+	public static final String ERROR_EDIT_INVALID_TASK_ID = "Invalid task ID entered";
+	public static final String ERROR_EDIT_NO_CHANGES = "No changes made for task %1$s";
 
 	// @@author A0125960E
 	// SAVE command
-	public static String SAVE_STORAGE = "storage";
-	public static String SAVE_LOG = "log";
-	public static String SAVE_DEFAULT = "default";
-	public static String MESSAGE_SAVE = "Saved %1$s file location: %2$s";
-	public static String ERROR_SAVE = "Error saving file location: %1$s";
-	public static String ERROR_SAVE_NO_LOCATION = "No %1$s file location specified";
-	public static String ERROR_SAVE_NO_CHANGES = "Same %1$s file location. No changes to %1$s file location: %2$s";
-	public static String ERROR_SAVE_COPY_FILE = "IOException while copying %1$s file to specified location: %2$s";
-	public static String ERROR_SAVE_DELETE_FILE = "IOException while deleting previous %1$s file: %2$s";
-	public static String ERROR_SAVE_FILE_ALREADY_EXISTS = "File already exists in specified location: %1$s";
+	public static final String SAVE_STORAGE = "storage";
+	public static final String SAVE_LOG = "log";
+	public static final String SAVE_DEFAULT = "default";
+	public static final String MESSAGE_SAVE = "Saved %1$s file location: %2$s";
+	public static final String ERROR_SAVE = "Error saving file location: %1$s";
+	public static final String ERROR_SAVE_NO_LOCATION = "No %1$s file location specified";
+	public static final String ERROR_SAVE_NO_CHANGES = "Same %1$s file location. No changes to %1$s file location: %2$s";
+	public static final String ERROR_SAVE_COPY_FILE = "IOException while copying %1$s file to specified location: %2$s";
+	public static final String ERROR_SAVE_DELETE_FILE = "IOException while deleting previous %1$s file: %2$s";
+	public static final String ERROR_SAVE_FILE_ALREADY_EXISTS = "File already exists in specified location: %1$s";
 
 	// @@author A0132764E
 	// SEARCH command
-	public static String ERROR_SEARCH_NO_PARAMETER = "No search parameters specified";
-	public static String HEADER_SEARCH = "Search results";
-	public static String MESSAGE_SEARCH = "%d match(es)";
-	public static String ERROR_SEARCH = "Error";
+	public static final String ERROR_SEARCH_NO_PARAMETER = "No search parameters specified";
+	public static final String HEADER_SEARCH = "Search results";
+	public static final String MESSAGE_SEARCH = "%d match(es)";
+	public static final String ERROR_SEARCH = "Error";
 
 	// @@author A0125990Y
 	// UNDO command
-	public static String MESSAGE_UNDO = "Undo success. Returning to previous state";
-	public static String ERROR_UNDO = "Undo fail: No more previous state";
-	public static String NO_MORE_UNDO = "No commands to undo";
-	public static String UNDO_ADD = "Undo previous ADD operation: ";
-	public static String UNDO_DELETE = "Undo previous DELETE operation: ";
-	public static String UNDO_EDIT = "Undo previous EDIT operation: ";
-	public static String UNDO_MARK = "Undo previous MARK operation: ";
+	public static final String MESSAGE_UNDO = "Undo success. Returning to previous state";
+	public static final String ERROR_UNDO = "Undo fail: No more previous state";
+	public static final String NO_MORE_UNDO = "No commands to undo";
+	public static final String UNDO_ADD = "Undo previous ADD operation: ";
+	public static final String UNDO_DELETE = "Undo previous DELETE operation: ";
+	public static final String UNDO_EDIT = "Undo previous EDIT operation: ";
+	public static final String UNDO_MARK = "Undo previous MARK operation: ";
 	
 	// INVALID command
-	public static String ERROR_INVALID_CMD = "Invalid command: %1$s";
+	public static final String ERROR_INVALID_CMD = "Invalid command: %1$s";
 	
 	// Header constants
-	public static String HEADER_DISPLAY = "Displaying %1$s tasks";
+	public static final String HEADER_DISPLAY = "Displaying %1$s tasks";
 	
 	// @@author A0125990Y
 	// Help Message 
-	public static String HEADER_HELP = " Displaying Help List";
-	public static String ERROR_HELP = " No such Help List";
+	public static final String HEADER_HELP = " Displaying Help List";
+	public static final String ERROR_HELP = " No such Help List";
 
 	// @@author A0126120B
 	// Enum values for the different statusbar statuses
